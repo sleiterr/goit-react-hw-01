f6dc5889166e7ecfcdd38fc7d0e0c2133d93fb29
Oleg Troian <sleite4r@gmail.com>
2024-08-05 23:25:54 +0200

    update components map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // App.jsx
 
 import "./Alert.css"
-import Profile from "./componets/Profil/Profil";
-import FriendsList from "./componets/FriendsList/FriendsList";
-import TransactionHistory from "./componets/TransactionHistory/TransactionHistory";
+import Profile from "./components/Profil/Profil";
+import FriendsList from "./components/FriendsList/FriendsList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import userData from "./data/userData.json";
 import friends from "./data/FriendsList.json";
 import transactions from './data/transactionHistory.json'
